77dfd9fbcbe0e3ba6b24616a28e5ca7678f80adf
Matthew Draper <matt_draper@outlook.com>
2019-10-25 16:39:41 -0500

    Initial commit

---
+++ dws-slider.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Draper Web Solutions Slider
+Plugin URI: https://github.com/matthewdraper/dws-wordpress-base-theme
+Description: This plugin creates a new customizable slider widget.
+Author: Draper Web Solutions
+Version: 0.0.1
+Author URI: https://draperwebsolutions.com
+*/
